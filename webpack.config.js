a3886293e1e21e1fbf5a10948b8d0a03eeb4f781
angelasubi <21978301@qq.com>
2017-09-21 09:30:16 +0800

    feat dev build dev-hrm config

--- webpack.config.js
+++ webpack.config.js
@@ -6,3 +6,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const Merge = require('webpack-merge')
 const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+
+
+let currentTarget = process.env.npm_lifecycle_event
+
+let debug, devServer, minimize
+
+if (currentTarget == 'build') {
+    debug = false, devServer = false, minimize = true
+} else if (currentTarget == 'dev') {
+    debug = true, devServer = false, minimize = false
+} else if (currentTarget == 'dev-hrm') {
+    debug = true, devServer = true, minimize = false
+}
